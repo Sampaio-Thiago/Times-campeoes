6b7dbf916296f48c808751f4963c8f08dddaf355
Sampaio-Thiago <thato_net@hotmail.com>
2020-07-10 00:08:29 -0300

    Pratica Java Orientado a objetos

---
+++ Times-campeões/src/CriaJogadores.java
@@ -0,0 +1,32 @@
+
+public class CriaJogadores {
+
+	public static void main(String[] args) {
+		Jogadores libertadores99 = new Jogadores();
+		libertadores99.goleiro = "Marcos";
+		libertadores99.lateralDireito = "Arce";
+		libertadores99.zagueiro1 = "Roque Jr.";
+		libertadores99.zagueiro2 = "Jr. Baiano";
+		libertadores99.lateralEsquerdo = "Júnior";
+		libertadores99.meioCampo1 = "CésarSampaio";
+		libertadores99.meioCampo2 = "Rogério";
+		libertadores99.meioCampo3 = "Alex";
+		libertadores99.meioCampo4 = "Zinho";
+		libertadores99.atacante = "Paulo Nunes";
+		libertadores99.centroavante = "Oséas";
+
+		System.out.println(libertadores99.goleiro);
+		System.out.println(libertadores99.lateralDireito);
+		System.out.println(libertadores99.zagueiro1);
+		System.out.println(libertadores99.zagueiro2);
+		System.out.println(libertadores99.lateralEsquerdo);
+		System.out.println(libertadores99.meioCampo1);
+		System.out.println(libertadores99.meioCampo2);
+		System.out.println(libertadores99.meioCampo3);
+		System.out.println(libertadores99.meioCampo4);
+		System.out.println(libertadores99.atacante);
+		System.out.println(libertadores99.centroavante);
+
+	}
+
+}
---
+++ Times-campeões/src/Jogadores.java
@@ -0,0 +1,15 @@
+
+public class Jogadores {
+	
+	String goleiro;
+	String lateralDireito;
+	String zagueiro1;
+	String zagueiro2;
+	String lateralEsquerdo;
+	String meioCampo1;
+	String meioCampo2;
+	String meioCampo3;
+	String meioCampo4;
+	String atacante;
+	String centroavante;
+}
